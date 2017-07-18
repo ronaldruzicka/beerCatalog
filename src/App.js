fbd4bf8e5464853a601a0cc823f29283c257f5e5
Ronald Ruzicka <I>
2017-07-18 19:32:36 +0200

    small setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import Catalog from './components/Catalog.js';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="container">
+        <div className="d-flex align-items-baseline">
+          <h1 className="font-weight-bold">Beer catalog</h1>
+          <h2 className="text-muted">find your beer</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Catalog />
       </div>
     );
   }
---
+++ src/components/Catalog.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Pagination from './Pagination';
+
+export default class Catalog extends React.Component {
+  render() {
+    return (
+      <div className="Catalog">
+        <Pagination />
+      </div>
+    );
+  }
+}
